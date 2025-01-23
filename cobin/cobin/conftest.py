11240bed4170ecbc97bc74771881d368cfb96580
Luvxy <93637069+Luvxy@users.noreply.github.com>
2025-01-23 13:53:55 +0900

    start project

---
+++ cobin/cobin/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from cobin.users.models import User
+from cobin.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()

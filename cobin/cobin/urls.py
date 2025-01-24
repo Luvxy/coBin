1b09a9c5996c2678bb61556fac4d72ee30edaf18
Luvxy <93637069+Luvxy@users.noreply.github.com>
2025-01-24 13:57:16 +0900

    done blog framework

--- cobin/cobin/urls.py
+++ cobin/cobin/urls.py
@@ -17,7 +17,18 @@ from django.contrib import admin
 from django.urls import path
 from cobin_app import views
 
+# 이미지를 업로드 하기 위한 설정
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('blog/', views.blog, name='blog'),
+    path('blog/<int:pk>/', views.posting, name="posting"),
+    path('blog/new_post/', views.new_post),
+    path('blog/<int:pk>/remove/', views.remove_post),
 ]
+
+# 이미지 URL 설정
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- cobin/cobin_app/views.py
+++ cobin/cobin_app/views.py
@@ -1,4 +1,43 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from .models import Post
 
 def home(request):
-    return render(request, 'index.html')  # 템플릿 파일 경로
+    return render(request, 'index.html')  # index 파일 경로
+
+def blog(request):
+    # 게시글을 전부 가져와 postlist에 저장
+    postlist = Post.objects.all()
+    # blog.html 파일을 불러올때 postlist도 같이 불러옴
+    return render(request, 'blog.html', {'postlist':postlist})  # blog 파일 경로
+
+# blog의 게시글(posting)을 부르는 posting 함수
+def posting(request, pk):
+    # 게시글(Post) 중 pk(primary_key)를 이용해 하나의 게시글(post)를 검색
+    post = Post.objects.get(pk=pk)
+    # posting.html 페이지를 열 때, 찾아낸 게시글(post)을 post라는 이름으로 가져옴
+    return render(request, 'posting.html', {'post':post})
+
+def new_post(request):
+    if request.method == 'POST':
+        if request.POST['mainphoto']:
+            new_article=Post.objects.create(
+                postname=request.POST['postname'],
+                contents=request.POST['contents'],
+                mainphoto=request.POST['mainphoto'],
+            )
+        else:
+            new_article=Post.objects.create(
+                postname=request.POST['postname'],
+                contents=request.POST['contents'],
+                mainphoto=request.POST['mainphoto'],
+            )
+        return redirect('/blog/')
+    return render(request, 'new_post.html')
+
+def remove_post(request, pk):
+    post = Post.objects.get(pk=pk)
+    if request.method == 'POST':
+        post.delete()
+        return redirect('/blog/')
+    return render(request, 'remove_post.html', {'post': post})
